f60fd3f4692c7fc95b2b5081a16ab6b5d7feea11
Sandhiya96 <sandhuajap@gmail.com>
2022-01-06 17:58:18 +0530

    bala added aline

--- src/org/sample/EmployeeDetails.java
+++ src/org/sample/EmployeeDetails.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class EmployeeDetails {
+	private void m2() {
+		System.out.println("bala added a method 2");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
