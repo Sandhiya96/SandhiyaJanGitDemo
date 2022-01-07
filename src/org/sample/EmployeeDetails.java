d8aed1a1e491e2aca490fe2e4020d3b606ebacc7
Sandhiya96 <sandhuajap@gmail.com>
2022-01-07 11:18:24 +0530

    barani

--- src/org/sample/EmployeeDetails.java
+++ src/org/sample/EmployeeDetails.java
@@ -7,6 +7,9 @@ public class EmployeeDetails {
 	private void m3() {
 		System.out.println("sam created a method 3");
 	}
+	private void m4() {
+		System.out.println(" barani");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
